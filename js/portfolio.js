d4748edfc57d5de004ff1e1dbe4220998f52aea5
Achi4b <chiriengonsirim@gmail.com>
2024-12-11 06:28:11 +0100

    javascript commit

---
+++ js/portfolio.js
@@ -0,0 +1,26 @@
+// declaring using var
+var x = 10;
+console.log(x);
+
+var x = 20;
+console.log(x);
+
+// using let
+
+let y = 20;
+console.log(y);
+// let y = 30;
+
+y = 40;
+console.log(y);
+
+// using const
+
+const z = 60;
+console.log(z);
+// z = 70;
+
+const obj ={name: "richy"};
+obj.name = "bob";
+console.log(obj.name);  
+
